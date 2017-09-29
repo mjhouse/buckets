f9a9c924261aab37fce9e57f1e7e378ad5b856e6
Michael House <michaelhouse@gmx.us>
2017-09-29 06:41:00 -0500

    working

--- src/buckets/ui/BucketsUI.java
+++ src/buckets/ui/BucketsUI.java
@@ -40,12 +40,15 @@ public class BucketsUI extends javax.swing.JFrame {
         watchAdd = new javax.swing.JButton();
         watchRemove = new javax.swing.JButton();
         rulesTab = new javax.swing.JPanel();
-        rulesInputLabel = new javax.swing.JLabel();
-        rulesRuleInput = new javax.swing.JTextField();
-        rulesAdd = new javax.swing.JButton();
-        rulesRemove = new javax.swing.JButton();
-        rulesRuleList = new javax.swing.JScrollPane();
-        rulesRuleInput1 = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jTextField2 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jLabel4 = new javax.swing.JLabel();
 
         filePicker.setFileSelectionMode(javax.swing.JFileChooser.DIRECTORIES_ONLY);
 
@@ -120,69 +123,78 @@ public class BucketsUI extends javax.swing.JFrame {
 
         Tabs.addTab("Watch", watchTab);
 
-        rulesInputLabel.setText("Add Rule");
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Add Rule");
 
-        rulesRuleInput.addActionListener(new java.awt.event.ActionListener() {
+        jLabel2.setText("Regex");
+
+        jLabel3.setText("Move To");
+
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                rulesRuleInputActionPerformed(evt);
+                jTextField1ActionPerformed(evt);
             }
         });
 
-        rulesAdd.setText("Add");
-        rulesAdd.addActionListener(new java.awt.event.ActionListener() {
+        jButton1.setText("...");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                rulesAddActionPerformed(evt);
+                jButton1ActionPerformed(evt);
             }
         });
 
-        rulesRemove.setText("Remove");
-        rulesRemove.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                rulesRemoveActionPerformed(evt);
-            }
-        });
+        jButton2.setText("Add");
 
-        rulesRuleList.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-
-        rulesRuleInput1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                rulesRuleInput1ActionPerformed(evt);
-            }
-        });
+        jLabel4.setText("<html>Add a regular expression and a location to move the matching file to.</html>");
+        jLabel4.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        jLabel4.setVerticalTextPosition(javax.swing.SwingConstants.TOP);
 
         javax.swing.GroupLayout rulesTabLayout = new javax.swing.GroupLayout(rulesTab);
         rulesTab.setLayout(rulesTabLayout);
         rulesTabLayout.setHorizontalGroup(
             rulesTabLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(rulesTabLayout.createSequentialGroup()
-                .addGap(14, 14, 14)
-                .addComponent(rulesInputLabel)
-                .addGap(18, 18, 18)
-                .addComponent(rulesRuleInput, javax.swing.GroupLayout.PREFERRED_SIZE, 227, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(rulesRuleInput1, javax.swing.GroupLayout.PREFERRED_SIZE, 204, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(rulesAdd)
-                .addGap(18, 18, 18)
-                .addComponent(rulesRemove)
-                .addContainerGap(25, Short.MAX_VALUE))
-            .addGroup(rulesTabLayout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(rulesRuleList)
+                .addGroup(rulesTabLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(rulesTabLayout.createSequentialGroup()
+                        .addGap(21, 21, 21)
+                        .addGroup(rulesTabLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel3))
+                        .addGap(40, 40, 40)
+                        .addGroup(rulesTabLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jTextField2, javax.swing.GroupLayout.DEFAULT_SIZE, 271, Short.MAX_VALUE)
+                            .addComponent(jTextField1))
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton1)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton2)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, 164, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         rulesTabLayout.setVerticalGroup(
             rulesTabLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(rulesTabLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(rulesTabLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(rulesRuleInput1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(rulesRuleInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(rulesInputLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(rulesAdd)
-                    .addComponent(rulesRemove))
+                .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(rulesRuleList, javax.swing.GroupLayout.DEFAULT_SIZE, 220, Short.MAX_VALUE)
+                .addGroup(rulesTabLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(rulesTabLayout.createSequentialGroup()
+                        .addGroup(rulesTabLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel2))
+                        .addGap(18, 18, 18)
+                        .addGroup(rulesTabLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jButton1)
+                            .addComponent(jLabel3)
+                            .addComponent(jButton2)))
+                    .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, 78, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 146, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -230,21 +242,17 @@ public class BucketsUI extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_watchAddActionPerformed
 
-    private void rulesRuleInputActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rulesRuleInputActionPerformed
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_rulesRuleInputActionPerformed
+    }//GEN-LAST:event_jTextField1ActionPerformed
 
-    private void rulesAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rulesAddActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_rulesAddActionPerformed
-
-    private void rulesRemoveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rulesRemoveActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_rulesRemoveActionPerformed
-
-    private void rulesRuleInput1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rulesRuleInput1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_rulesRuleInput1ActionPerformed
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        int result = filePicker.showOpenDialog(this);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            String file = filePicker.getSelectedFile().getAbsolutePath();
+            jTextField2.setText(file);
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -284,12 +292,15 @@ public class BucketsUI extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTabbedPane Tabs;
     private javax.swing.JFileChooser filePicker;
-    private javax.swing.JButton rulesAdd;
-    private javax.swing.JLabel rulesInputLabel;
-    private javax.swing.JButton rulesRemove;
-    private javax.swing.JTextField rulesRuleInput;
-    private javax.swing.JTextField rulesRuleInput1;
-    private javax.swing.JScrollPane rulesRuleList;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
     private javax.swing.JPanel rulesTab;
     private javax.swing.JButton watchAdd;
     private javax.swing.JTextField watchDirectoryInput;
